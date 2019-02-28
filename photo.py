ea340f40f887058ccc2ef3d02c9ea595de6e8c58
NBerlin <nickiberlinpolhem@gmail.com>
2019-02-28 19:04:05 +0100

    photo class

---
+++ photo.py
@@ -0,0 +1,12 @@
+class photo():
+    def __init__(self,tags,ori):
+        self.tags=tags
+        self.ori=ori
+    def getTags(self):
+        return self.tags
+
+    def getOrientation(self):
+        return self.ori;
+
+    def getPoints(self,tags2):
+        
