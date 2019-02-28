6e8e91f5e9a3e96bee25db448a775dffec98b2f5
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2019-02-28 20:49:10 +0100

    update photo

--- photo.py
+++ photo.py
@@ -4,14 +4,19 @@ class photo():
         self.isVert = isVert
         self.index = index
         self.isUsed = False
+        self.other = None
 
     def getTags(self):
-        return self.tags
+        if self.other is not None:
+            return list(set(self.tags + self.other.tags))
+        return self.tags 
 
     def isVertical(self):
         return self.isVert
 
     def getIndex(self):
+        if self.other is not None:
+            return str(self.other.index) + ' ' + str(self.index)
         return self.index
 
     def isUsed(self):
@@ -19,6 +24,17 @@ class photo():
 
     def use(self):
         self.isUsed = True
+    
+    def merge(self, other):
+        if not self.isVert or not other.isVertical():
+            raise Exception('only merge verts!!')
+        self.other = other
+
+    def isMerged(self):
+        return self.other is not None
 
     def setOtherPhotos(self,otherPhotos):
         self.otherPhotos=otherPhotos
+     
+    def __str__(self):
+        return str(self.getIndex()) + str(self.getTags())
